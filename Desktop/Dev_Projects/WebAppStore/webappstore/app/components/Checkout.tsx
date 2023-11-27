a30e34e2944ade25eb9d48fe1699c752d6992f89
MarkMagorimbo <you@example.com>
2023-11-27 16:46:59 +0800

    checkout form edits

--- Desktop/Dev_Projects/WebAppStore/webappstore/app/components/Checkout.tsx
+++ Desktop/Dev_Projects/WebAppStore/webappstore/app/components/Checkout.tsx
@@ -105,7 +105,7 @@ const CheckoutForm = (item: any) => {
                     <div className="mt-6 border-t border-b py-2">
                         <div className="flex items-center justify-between">
                             <p className="text-sm font-medium text-gray-900">Subtotal</p>
-                            <p className="font-semibold text-gray-900">$399.00</p>
+                            <p className="font-semibold text-gray-900">$----</p>
                         </div>
                         <div className="flex items-center justify-between">
                             <p className="text-sm font-medium text-gray-900">Shipping</p>
@@ -114,7 +114,7 @@ const CheckoutForm = (item: any) => {
                     </div>
                     <div className="mt-6 flex items-center justify-between">
                         <p className="text-sm font-medium text-gray-900">Total</p>
-                        <p className="text-2xl font-semibold text-gray-900">$408.00</p>
+                        <p className="text-2xl font-semibold text-gray-900">$----</p>
                     </div>
                 </div>
                 <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">Place Order</button>
